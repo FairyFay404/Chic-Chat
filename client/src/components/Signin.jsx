1ee086fb1bbe5d085033a0abbf98a109ccdd9589
PJ-Meaw <pathinya19@gmail.com>
2023-04-12 00:22:40 +0700

    Finished Signin Page (Frontend)

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Signin() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function toRegister(){
+        useNavigate("/Register");
+    }
+
+    function handleSignin(){
+        console.log(username + password);
+        
+    }
+
+    
     return (
         <>
-            <div className="bg-gradient-to-b from-[#1565D8] to-[#9EE8FF] h-screen">
+            <div className="bg-gradient-to-b from-[#1565D8] to-[#9EE8FF] h-screen ">
                 <div className="w-full h-screen bg-no-repeat bg-cover" style={{backgroundImage: `url("/Wallpaper.png")`}}>
                     <div className="flex flex-row justify-center items-center h-full">
-                        <div className="w-[850px] h-[682px] rounded-[10px] bg-gradient-to-b from-[#C9F2FF] from-0% to-[#E9FAFF] to-67%">
-                            <div className="flex flex-col">
-                                <p className="text-[#1565D8] font-Montserrat font-bold">abasd</p>
+                        <div className="w-[850px] h-[682px] rounded-[50px] bg-gradient-to-b from-[#C9F2FF] from-0% to-[#E9FAFF] to-67%">
+                            <div className="flex flex-col items-center mt-[80px]">
+                                <p className="text-[#1565D8] font-Montserrat font-bold text-[52px] leading-[130%]">Sign in</p>
+                                <p className="text-[#072653] font-Rubik font-normal leading-[19px] mt-[8px]">Welcome to ChicChat ! Let's talk !</p>
+                                <p className="after:content-['*'] after:ml-0.5 after:text-red-500
+                                    text-[#000000] font-Rubik font-medium text-[18px] leading-[120%] self-start ml-[108px] mt-[39px]
+                                ">Username</p>
+                                <input type="text" className="w-[680px] h-[76px] pl-[28px] rounded-[20px] shadow_1 mt-[11px] 
+                                    text-[#072653] font-Rubik font-medium border-[0px] focus:border-[1px] focus:border-[#178AAE] focus:outline-0
+                                    " placeholder="Enter your username" onChange={e => setUsername(e.target.value)}/>
+                                <p className="after:content-['*'] after:ml-0.5 after:text-red-500
+                                    text-[#000000] font-Rubik font-medium text-[18px] leading-[120%] self-start ml-[108px] mt-[28px]
+                                    ">Password</p>
+                                <input type="password" className="w-[680px] h-[76px] pl-[28px] rounded-[20px] shadow_1 mt-[11px] 
+                                    text-[#072653] font-Rubik font-medium border-[0px] focus:border-[1px] focus:border-[#178AAE] focus:outline-0
+                                    " placeholder="Enter your password"  onChange={e => setPassword(e.target.value)}/>
+                                <button className="w-[170px] h-[59px] rounded-[50px] mt-[90px] bg-gradient-to-b from-[#072653] from-3% via-[#1565D8] via-40% to-[#2FBCE8] to-96%
+                                    text-[#FFFFFF] font-Montserrat font-semibold text-[20px] leading-[24px] cursor-pointer
+                                    hover:border-[2px] hover:border-[#178AAE]
+                                    " onClick={handleSignin}>Sign in</button>
+                                <p className="mt-[20px] text-[#696F79] font-Rubik font-normal text-[14px]">I don’t have an account? <span className="text-[#1565D8] underline cursor-pointer" onClick={toRegister}>Register </span></p>
                             </div>
                         </div>
                     </div>
