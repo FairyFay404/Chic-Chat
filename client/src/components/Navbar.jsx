979ee64a15d3de323922da4343a64b275ca885da
PJ-Meaw <pathinya19@gmail.com>
2023-04-20 18:08:12 +0700

    update-chat

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,23 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
-    
+
     return (
         <>
-            <div className="w-[1920px] h-[100px] bg-white ] flex justify-between items-center">
-                <div className="flex items-center ">
-                    <img className='ms-[56px]' src="/ChicChatLOGO.png" alt="" />
-                    <h1 className=' text-[30px] font-semibold text-[#1565D8]'>ChicChat</h1>
+            <div className="relative">
+                <div className="fixed top-0 left-0 w-full h-[100px] bg-white">
+                    <div className="max-w-[1920px] mx-auto h-full">
+                        <div className="flex flex-row h-full justify-between items-center">
+                            <div className="flex items-center">
+                                <img className='ms-[56px]' src="/ChicChatLOGO.png" alt="" />
+                                <h1 className=' text-[30px] font-semibold text-[#1565D8]'>ChicChat</h1>
+                            </div>
+                            <button className='me-[51px] w-[170px] h-[59px] rounded-[50px] bg-gradient-to-b from-[#DB3D3D] via-[#FC6262] to-[#FF9595] font-semibold text-[20px] text-white font-Montserrat hover:border-[2px] hover:border-[#f02e2e]'>Log out</button>
+                        </div>
+                    </div>
                 </div>
-                <button className='me-[51px] w-[170px] h-[59px] rounded-[50px] bg-gradient-to-b from-[#DB3D3D] via-[#FC6262] to-[#FF9595] font-semibold text-[20px] text-white font-Montserrat hover:border-[2px] hover:border-[#f02e2e]'>Log out</button>
             </div>
+
         </>
     )
 }
