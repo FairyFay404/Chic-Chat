569820a25b7a7d4613e7f921f5441fbc6d82e0d6
PJ-Meaw <pathinya19@gmail.com>
2023-04-30 22:21:02 +0700

    Add link of Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+        alert("logout")
+        navigate('/')
+        // Delete Token
+        // 
+    }
+
+    const backToHome = () => {
+        alert("back to home")
+        navigate('/home')
+        // 
+    }
 
     return (
         <>
@@ -8,11 +23,11 @@ export default function Navbar() {
                 <div className="fixed top-0 left-0 w-full h-[100px] bg-white">
                     <div className="max-w-[1920px] mx-auto h-full">
                         <div className="flex flex-row h-full justify-between items-center">
-                            <div className="flex items-center">
+                            <div className="flex items-center cursor-pointer" onClick={backToHome}>
                                 <img className='ms-[56px]' src="/ChicChatLOGO.png" alt="" />
                                 <h1 className=' text-[30px] font-semibold text-[#1565D8]'>ChicChat</h1>
                             </div>
-                            <button className='me-[51px] w-[170px] h-[59px] rounded-[50px] bg-gradient-to-b from-[#DB3D3D] via-[#FC6262] to-[#FF9595] font-semibold text-[20px] text-white font-Montserrat hover:border-[2px] hover:border-[#f02e2e]'>Log out</button>
+                            <button className='me-[51px] w-[170px] h-[59px] rounded-[50px] bg-gradient-to-b from-[#DB3D3D] via-[#FC6262] to-[#FF9595] font-semibold text-[20px] text-white font-Montserrat hover:border-[2px] hover:border-[#f02e2e]' onClick={handleLogout}>Log out</button>
                         </div>
                     </div>
                 </div>
